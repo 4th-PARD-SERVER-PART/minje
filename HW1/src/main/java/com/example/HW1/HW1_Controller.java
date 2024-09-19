299ad17a019044fd5dc7af91f2a97322088bee57
MinJe Kim <minje9370@handong.ac.kr>
2024-09-19 17:53:25 +0900

    Your commit message

--- HW1/src/main/java/com/example/HW1/HW1_Controller.java
+++ HW1/src/main/java/com/example/HW1/HW1_Controller.java
@@ -1,4 +1,17 @@
 package com.example.HW1;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HW1_Controller {
+
+    @RequestMapping("/path/{name}/{age}/{school}/{major}/{mbti}")
+    public String path(@PathVariable String name, @PathVariable int age
+    , @PathVariable int school, @PathVariable int major, @PathVariable int mbti) {
+        return "안녕하세요. 저의 자기소개를 하겠습니다. 저의 이름은 " + name + " 입니다. 저는 " + age + "입니다. 학교는 " + school +
+                "이고, 전공은 " + major + "입니다. 마지막으로 저의 mbti는 " + mbti + "입니다. 감사합니다.";
+    }
+
 }
