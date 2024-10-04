dbe5d5570845bc6d55f92d115eb61838e1c33ec0
MinJe Kim <minje9370@handong.ac.kr>
2024-10-04 13:48:04 +0900

    hwhw

--- thridseminar/src/main/java/com/example/thridseminar/user/repository/UserRepository.java
+++ thridseminar/src/main/java/com/example/thridseminar/user/repository/UserRepository.java
@@ -1,4 +1,10 @@
 package com.example.thridseminar.user.repository;
 
-public interface UserRepository {
+import com.example.thridseminar.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    public User findByEmail(String email);
 }
